29600f821bac56890a12f95c78c9f71601662961
Huib Piguillet <huib@monetor.com>
2024-02-26 02:45:01 +0100

    Extendable ConfigLoader class

--- src/powerchord/config.py
+++ src/powerchord/config.py
@@ -1,8 +1,9 @@
 import argparse
 import tomllib
-from collections.abc import Callable
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from pathlib import Path
+from typing import ClassVar
 
 from chili import decode
 from gaffe import raises
@@ -13,6 +14,55 @@ from .runner import Task
 
 class ParseConfigError(Exception):
     pass
+
+
+@dataclass
+class Config:
+    tasks: list[Task] = field(default_factory=list)
+    log_levels: LogLevels = field(default_factory=LogLevels)
+
+
+class DecodeConfigError(Exception):
+    pass
+
+
+@dataclass
+class ConfigLoader(ABC):
+    name: ClassVar[str]
+
+    tasks: list[Task] = field(default_factory=list)
+    log_levels: LogLevels = field(default_factory=LogLevels)
+
+    @classmethod
+    @raises(ParseConfigError)
+    @abstractmethod
+    def _parse(cls) -> dict:
+        pass
+
+    @classmethod
+    @raises(ParseConfigError, DecodeConfigError)
+    def load(cls) -> Config | None:
+        config_dict = cls._parse()
+        if not any(config_dict.values()):
+            return None
+
+        tasks = config_dict.get('tasks', {})
+        if isinstance(tasks, list):
+            task_items = [('', t) if isinstance(t, str) else t for t in tasks]
+        elif isinstance(tasks, dict):
+            task_items = list(tasks.items())
+        else:
+            raise DecodeConfigError(f'Wrong value for tasks: {tasks}')
+        config_dict['tasks'] = [{'command': t, 'name': n} for n, t in task_items]
+
+        log_levels = config_dict.get('log_levels', {})
+        if isinstance(log_levels, list):
+            config_dict['log_levels'] = dict(log_levels)
+
+        try:
+            return decode(config_dict, Config, decoders={LogLevel: LogLevel})
+        except ValueError as exc:
+            raise DecodeConfigError(*exc.args) from exc
 
 
 def parse_key_value_pair(value: str) -> tuple[str, str]:
@@ -27,75 +77,50 @@ def try_parse_key_value_pair(value: str) -> str | tuple[str, str]:
         return value
 
 
-@raises(ParseConfigError)
-def config_from_args(_config_source: str) -> dict:
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument(
-        '-t',
-        '--tasks',
-        dest='tasks',
-        nargs='+',
-        metavar='COMMAND | NAME=COMMAND',
-        type=try_parse_key_value_pair,
-        default={},
-    )
-    arg_parser.add_argument(
-        '-l',
-        '--log-levels',
-        dest='log_levels',
-        nargs='+',
-        metavar='OUTPUT=LOGLEVEL (debug | info | warning | error | critical | "")',
-        type=parse_key_value_pair,
-        default={},
-    )
-    try:
-        return arg_parser.parse_args().__dict__
-    except (SystemExit, TypeError) as exc:
-        raise ParseConfigError from exc
+class CLIConfig(ConfigLoader):
+    name = 'command line'
+
+    @classmethod
+    @raises(ParseConfigError)
+    def _parse(cls) -> dict:
+        arg_parser = argparse.ArgumentParser()
+        arg_parser.add_argument(
+            '-t',
+            '--tasks',
+            dest='tasks',
+            nargs='+',
+            metavar='COMMAND | NAME=COMMAND',
+            type=try_parse_key_value_pair,
+            default={},
+        )
+        arg_parser.add_argument(
+            '-l',
+            '--log-levels',
+            dest='log_levels',
+            nargs='+',
+            metavar='OUTPUT=LOGLEVEL (debug | info | warning | error | critical | "")',
+            type=parse_key_value_pair,
+            default={},
+        )
+        try:
+            return arg_parser.parse_args().__dict__
+        except (argparse.ArgumentError, SystemExit, TypeError) as exc:
+            raise ParseConfigError from exc
 
 
-@raises(ParseConfigError)
-def config_from_pyproject(config_source: str) -> dict:
-    try:
-        with Path(config_source).open('rb') as f:
-            return tomllib.load(f).get('tool', {}).get('powerchord', {})
-    except OSError:
-        return {}
-    except ValueError as exc:
-        raise ParseConfigError from exc
+class PyprojectConfig(ConfigLoader):
+    name = 'pyproject.toml'
 
-
-@dataclass
-class Config:
-    tasks: list[Task] = field(default_factory=list)
-    log_levels: LogLevels = field(default_factory=LogLevels)
-
-
-class DecodeConfigError(Exception):
-    pass
-
-
-@raises(DecodeConfigError)
-def decode_config(value: dict) -> Config | None:
-    if not any(value.values()):
-        return None
-    tasks = value.get('tasks', {})
-    if isinstance(tasks, list):
-        task_items = [('', t) if isinstance(t, str) else t for t in tasks]
-    elif isinstance(tasks, dict):
-        task_items = list(tasks.items())
-    else:
-        raise DecodeConfigError(f'Wrong value for tasks: {tasks}')
-    value['tasks'] = [{'command': t, 'name': n} for n, t in task_items]
-
-    log_levels = value.get('log_levels', {})
-    if isinstance(log_levels, list):
-        value['log_levels'] = dict(log_levels)
-
-    try:
-        return decode(value, Config, decoders={LogLevel: LogLevel})
-    except ValueError as exc:
-        raise DecodeConfigError(*exc.args) from exc
+    @classmethod
+    @raises(ParseConfigError)
+    def _parse(cls) -> dict:
+        try:
+            with Path('pyproject.toml').open('rb') as f:
+                return tomllib.load(f).get('tool', {}).get('powerchord', {})
+        except OSError:
+            return {}
+        except ValueError as exc:
+            raise ParseConfigError from exc
 
 
 class LoadConfigError(Exception):
@@ -108,19 +133,13 @@ class LoadSpecificConfigError(LoadConfigError):
         super().__init__(f' from {name}', *args)
 
 
-CONFIG_LOADERS: dict[str, Callable[[str], dict]] = {
-    'command line': config_from_args,
-    'pyproject.toml': config_from_pyproject,
-}
-
-
 @raises(LoadConfigError)
-def load_config() -> Config:
-    for name, loader in CONFIG_LOADERS.items():
+def load_config(*loaders: type[ConfigLoader]) -> Config:
+    for loader_cls in loaders:
         try:
-            config = decode_config(loader(name))
-        except ParseConfigError as exc:
-            raise LoadSpecificConfigError(name, *exc.args) from exc
+            config = loader_cls.load()
+        except (ParseConfigError, DecodeConfigError) as exc:
+            raise LoadSpecificConfigError(loader_cls.name, *exc.args) from exc
         if config:
             return config
     raise LoadConfigError
